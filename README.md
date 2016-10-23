eefc81b8ea627e6e1d27f2142e592b43cfbd01ea
pmsosa <sosavpm@gmail.com>
2016-10-22 23:59:06 -0700

    1. Added Policy Modes
    2. Added Logging
    
    3. Added sneaky policy mode
    
    4. Fixed issues w/ password protection
    
    5. Updated readme

--- README.md
+++ README.md
@@ -7,7 +7,30 @@
 <h3>Intro</h3>
 [Rubberduckies](https://hakshop.myshopify.com/products/usb-rubber-ducky-deluxe) are small usb devices that pretend to be usb keyboards and can type on their own at very high speeds. Because most -if not all- OS trust keyboards automatically, it is hard to protect oneself from these attacks.
 
-**DuckHunt** is a small efficient script that acts as a daemon consistently monitoring your keyboard usage (right now, only speed is monitored) that can catch and prevent a rubber ducky attack.
+**DuckHunt** is a small efficient script that acts as a daemon consistently monitoring your keyboard usage (right now, speed and selected window) that can catch and prevent a rubber ducky attack. (Technically it helps prevent any type of automated keystroke injection attack, so things like Mousejack injections are also covered.)
+
+
+
+<h3>Features</h3>
+**Protection Policy**
+ - **Paranoid:** When an attack is detected, keyboard input is disallowed until a password is input. Attack will also be logged.
+ - **Normal:** When an attack is detected, keyboard input will temporarily be disallowed. (After it is deemed that the treat is over, keyboard input will be allowed again). Attack will also be logged.
+ - **Sneaky:** When an attacks is detected, a few keys will be dropped (enough to break any attack, make it look as if the attacker messed up.) Attack will also be logged.
+ - **LogOnly:** When an attack is detected, simply log the attack and in no way stop it. 
+
+**Extras**
+ - Program Blacklist: If there are specific programs you neve use (cmd, powershell). Consider interactions with them as highly suspecious and take action based on the protection policy.
+
+<h3>Setup</h3>
+- **Regular users**:
+ - OPT#1: Normal Protection w/ Program Blacklisting for Commandline and Powershell:
+  - [TODO] Simply download these files and run install.exe
+ - OPT#2: Normal Protection (w/o any blacklisting):
+  - [TODO] Simply download these files and run install.exe
+
+
+- **Advanced Users**
+ - Keep Reading...
 
 <h3>Requirements</h3>
  
@@ -15,10 +38,9 @@
 - [PyHook](https://sourceforge.net/projects/pyhook/)
 - [Py2Exe](http://py2exe.org/)
 
-
-<h3>Setup</h3>
-- Step 1. Customize duckhunt.py variables to your desire
- - You can customize the password, speed threshold, enable/disable password protection, etc.
+<h3>Advanced Setup</h3>
+- Step 1. Customize duckhunt.conf variables to your desire
+ - You can customize the password, speed threshold, privacy, etc.
 
 - Step 2. Turn the duckhunt**.py** to a duckhunt**.pyw** so that the console doesn't show up when you run the program
 
@@ -26,16 +48,10 @@
 
 - Step 4. Run the program. You are now protected from RubberDuckies!
 
-<h3>Current Features</h3>
-- **Basic Ducky Protection:** If a rubber ducky is detected, all keystrokes deemed to be from the ducky will be ignored.
-- **Password Protection:** If enabled, once a rubber ducky attack is detected, any and all future keystrokes will be ignored until a (specified) password is entered.
-
 <h3>TODO</h3>
 - More monitoring features: 
- - Monitor the windows where you type (eg. cmdline, regedit, run)
+ - Add OSX & Linux support!
  - Look for certain patterns (eg. "GUI D, GUI R, cmd, ENTER")
- - specific commands (eg. powershell)
-- Read from .conf file
  
  <h1>Happy Hunting!</h1>
 ![Duck Hunt](http://konukoii.com/blog/wp-content/uploads/2016/10/duck-hunt.jpg)
--- duckhunt.py
+++ duckhunt.py
@@ -18,12 +18,14 @@ duckhunt = imp.load_source('duckhunt', 'duckhunt.conf')
 
 ##### NOTES #####
 #
-# 1. Undestanding Password Protection Policy:
-#   - Password Protection  ON : When a rubber ducky attack is detected, lock down any further keypresses until the correct password is entered.
-#   - Without Password Protection: When a rubber ducky attack is detected, it will lock down any further keypresses until the time spacing between keys goes above the threshold.
-#
+# 1. Undestanding Protection Policy:
+#   - Paranoid: When an  attack is detected, lock down any further keypresses until the correct password is entered. (set password in .conf file). Attack will also be logged.
+#   - Normal :  When an attack is detected, keyboard input will temporarily be disallowed. (After it is deemed that the treat is over, keyboard input will be allowed again). Attack will also be logged.
+#   - Sneaky: When an attacks is detected, a few keys will be dropped (enough to break any attack, make it look as if the attacker messed up.) Attack will also be logged.
+#   - LogOnly: When an attack is detected, simply log the attack and in no way stop it. 
+
 # 2. How To Use
-#   - Modify the user configurable vars below. (particularly pwdprotect and password)
+#   - Modify the user configurable vars below. (particularly policy and password)
 #   - Turn the program into a .pyw to run it as windowless script.
 #   - (Opt) Use py2exe to build an .exe
 #
@@ -33,35 +35,88 @@ duckhunt = imp.load_source('duckhunt', 'duckhunt.conf')
 
 
 
-threshold  = duckhunt.threshold   # Speed Threshold
-size       = duckhunt.size        # Size of history array
-pwdprotect = duckhunt.pwdprotect  # Enable/Disable Password Protection Feature
-password   = duckhunt.password    # Password used in Password Protection mode
-pcounter   = 0                    # Password Counter (If using password)
-speed      = 0                    # Current Average Keystroke Speed
-prevTime   = -1                   # Previous Keypress Timestamp
-i          = 0                    # History Array Timeslot
-intrusion  = False                # Boolean Flag to be raised in case of intrusion detection
-history    = [threshold+1] * size #Array for keeping track of average speeds across the last n keypresses
+threshold  = duckhunt.threshold      # Speed Threshold
+size       = duckhunt.size           # Size of history array
+policy     = duckhunt.policy.lower() # Designate Policy Type
+password   = duckhunt.password       # Password used in Paranoid Mode
+################################################################################
+pcounter   = 0                       # Password Counter (If using password)
+speed      = 0                       # Current Average Keystroke Speed
+prevTime   = -1                      # Previous Keypress Timestamp
+i          = 0                       # History Array Timeslot
+intrusion  = False                   # Boolean Flag to be raised in case of intrusion detection
+history    = [threshold+1] * size    # Array for keeping track of average speeds across the last n keypresses
+randdrop   = duckhunt.randdrop       # How often should one drop a letter (in Sneaky mode)
+prevWindow = []                      # What was the previous window
+filename   = duckhunt.filename       # Filename to save attacks
+blacklist  = duckhunt.blacklist       # Program Blacklist
 
 
+
+#Logging the Attack
+def log(event):
+    global prevWindow
+
+    x = open(filename,"a+")
+    if (prevWindow != event.WindowName):
+        x.write ("\n[ %s ]\n" % (event.WindowName))
+        prevWindow =event.WindowName
+    if event.Ascii > 32 and event.Ascii < 127:
+        x.write(chr(event.Ascii))
+    else:
+        x.write("[%s]" % event.Key)
+        x.close()
+    return
+
+
+def caught(event):
+    global intrusion, policy, randdrop
+    print "Quack! Quack! -- Time to go Duckhunting!"
+    intrusion = True;
+
+    #Paranoid Policy
+    if (policy == "paranoid"):
+        win32ui.MessageBox("Someone might be trying to inject keystrokes into your computer.\nPlease check your ports or any strange programs running.\nEnter your Password to unlock keyboard.", "KeyInjection Detected",4096) # MB_SYSTEMMODAL = 4096 -- Always on top.
+        return False;
+    #Sneaky Policy
+    elif (policy == "sneaky"):
+        randdrop += 1 
+        #Drop every 5th letter
+        if (randdrop==7):
+            randdrop = 0;
+            return False;
+        else:
+            return True;
+
+    #Logging Only Policy
+    elif (policy == "log"):
+        log(event)
+        return True;
+
+
+    #Normal Policy
+    log(event)
+    return False
 
 
 #This is triggered every time a key is pressed
 def KeyStroke(event):
 
-    global threshold, pwdprotect, password, pcounter
-    global speed, prevTime, i, history, intrusion
+    global threshold, policy, password, pcounter
+    global speed, prevTime, i, history, intrusion,blacklist
+
+    print event.Key;
 
 
     #If an intrusion was detected and we are password protecting
     #Then lockdown any keystroke and until password is entered
-    if (pwdprotect & intrusion):    
+    if (policy == "paranoid" and intrusion):    
         print event.Key;
+        log(event);
         if (password[pcounter] == chr(event.Ascii)):
             pcounter += 1;
             if (pcounter == len(password)):
-                print "Correct Password -- Unlocking!"
+                win32ui.MessageBox("Correct Password!", "KeyInjection Detected",4096) # MB_SYSTEMMODAL = 4096 -- Always on top.
                 intrusion = False
                 pcounter = 0
         else:
@@ -86,16 +141,17 @@ def KeyStroke(event):
     i=i+1
 
     print "\rAverage Speed:",speed
+    
+    #Blacklisting    
+    for window in blacklist.split(","):
+        if window in event.WindowName:
+            return caught(event)
 
     #Intrusion detected
     if (speed < threshold):
-        print "Quack! Quack! -- Time to go Duckhunting!"
-        intrusion = True; 
-        if (pwdprotect):
-            win32ui.MessageBox("Someone might be trying to inject keystrokes into your computer.\nPlease check your ports or any strange programs running.\nEnter your Password to unlock keyboard.", "KeyInjection Detected",4096) # MB_SYSTEMMODAL = 4096 -- Always on top.
-        return False
-
-
+        return caught(event)
+    else:
+        intrusion = False
     # pass execution to next hook registered 
     return True
 
