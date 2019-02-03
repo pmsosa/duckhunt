2e0719712046130cff0759c6b2992dccc1afbb71
Kai Piper <kai9987kai@gmail.com>
2019-02-03 14:31:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 import py2exe, sys, os
-
+#This to change the script to an exe edit the variables inside the duckhunt-configurable.py file then run this script
 sys.argv.append('py2exe')
 
 setup(
@@ -9,7 +9,7 @@ setup(
     description = 'duckhunt-',
 
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows = [{'script': "duckhunt.py"}],
+    windows = [{'script': "duckhunt-configurable.py"}],
     zipfile = None,
 )
 
